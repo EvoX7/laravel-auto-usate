a163ed2c7fe8864cf11d0fe7e24a729cf6df00f1
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-12 16:46:08 +0200

    change text on header.blade

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -5,7 +5,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="#">HOME</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">AUTO</a>
