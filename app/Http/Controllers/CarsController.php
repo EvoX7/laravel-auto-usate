26dec322944d484887f43049eece75f97211730a
Rossiitaa <rosario.scuotto@outlook.it>
2022-09-12 17:25:14 +0200

    controller

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Car;
 use Illuminate\Http\Request;
 
 class CarsController extends Controller
 {
-    //
+    public function index() {
+        $cars = Car::all();
+        return view('car', compact('cars'));
+    }
 }
