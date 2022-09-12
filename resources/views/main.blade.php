1fdf76a2eb4e54f53e901f8529ab33d0a7c596ec
Crisano97 <97keb6@gmail.com>
2022-09-12 17:11:53 +0200

    main blade content

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,11 +2,12 @@
 
 @section('main-content')
     <h1>Auto usate</h1>
-
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    
+    @foreach ($cars as car)
+        <span>{{ $car->car_name }}</span>
+        <span>{{ $car->brand }}</span>
+        <span>{{ $car->color}}</span>
+        <span>{{ $car->trasmission }}</span>
+        <span>{{ $car->mileage }}</span> 
+        <span>{{ $car->price }}</span>     
+    @endforeach
 @endsection
