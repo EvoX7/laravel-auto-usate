a88a61b1f67849292a6c284a542872bd5ebf8ea2
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-12 16:31:17 +0200

    created footer

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,30 @@
+<footer class="">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">auto</a>
+                    </li>
+                    <li>
+                        <a href="#">Contatti</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-6">
+                <h5>inviaci un commento</h5>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                        placeholder="name@example.com">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </div>
+        </div>
+</footer>
